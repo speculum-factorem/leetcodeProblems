ae2678c23f051c2ddcaa038e47aa0c9352d0adb2
maxim <f4gbure@yandex.ru>
2024-12-22 20:22:53 +0300

    added 349's problem

---
+++ 349.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var varic1 = []int{1, 2, 3, 4}
+	var varic2 = []int{1, 2, 3}
+
+	fmt.Println(intersection(varic1, varic2))
+
+}
+
+func intersection(nums1 []int, nums2 []int) []int {
+
+	elMap := make(map[int]bool)
+
+	for _, num := range nums1 {
+
+		elMap[num] = true
+	}
+
+	result := []int{}
+
+	for _, num := range nums2 {
+
+		if _, value := elMap[num]; value {
+
+			result = append(result, num)
+
+			delete(elMap, num)
+		}
+
+	}
+
+	return result
+
+}
--- 419.go
+++ 419.go
@@ -1,52 +1,52 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	board := [][]byte{
-		{'X', '.', '.', 'X'},
-		{'.', '.', '.', 'X'},
-		{'.', '.', '.', 'X'},
-	}
+// func main() {
+// 	board := [][]byte{
+// 		{'X', '.', '.', 'X'},
+// 		{'.', '.', '.', 'X'},
+// 		{'.', '.', '.', 'X'},
+// 	}
 
-	count := countBattleships(board)
-	fmt.Println(count) // Вывод количества кораблей
-}
+// 	count := countBattleships(board)
+// 	fmt.Println(count) // Вывод количества кораблей
+// }
 
-func countBattleships(board [][]byte) int {
+// func countBattleships(board [][]byte) int {
 
-	countOfBattleships := 0
-	n := len(board)
-	m := len(board[0])
+// 	countOfBattleships := 0
+// 	n := len(board)
+// 	m := len(board[0])
 
-	for i := 0; i < n; i++ {
+// 	for i := 0; i < n; i++ {
 
-		for j := 0; j < m; j++ {
+// 		for j := 0; j < m; j++ {
 
-			if board[i][j] == 'X' {
-				countOfBattleships++
-				dfs(board, i, j)
-			}
+// 			if board[i][j] == 'X' {
+// 				countOfBattleships++
+// 				dfs(board, i, j)
+// 			}
 
-		}
-	}
-	return countOfBattleships
+// 		}
+// 	}
+// 	return countOfBattleships
 
-}
+// }
 
-func dfs(board [][]byte, i int, j int) {
+// func dfs(board [][]byte, i int, j int) {
 
-	if i < 0 || i >= len(board) || j < 0 || j >= len(board[0]) || board[i][j] == '.' {
-		return
-	}
+// 	if i < 0 || i >= len(board) || j < 0 || j >= len(board[0]) || board[i][j] == '.' {
+// 		return
+// 	}
 
-	board[i][j] = '.'
+// 	board[i][j] = '.'
 
-	dfs(board, i+1, j)
-	dfs(board, i-1, j)
-	dfs(board, i, j+1)
-	dfs(board, i, j-1)
+// 	dfs(board, i+1, j)
+// 	dfs(board, i-1, j)
+// 	dfs(board, i, j+1)
+// 	dfs(board, i, j-1)
 
-}
+// }
