34856bc2623d8e592ec4f1082bc91eeffea36c11
maxim <f4gbure@yandex.ru>
2024-12-22 18:10:10 +0300

    added 419's problem

--- 35.go
+++ 35.go
@@ -1,30 +1,30 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
+// func main() {
 
-	randArr := []int{1, 2, 5}
+// 	randArr := []int{1, 2, 5}
 
-	fmt.Println(searchInsert(randArr, 6))
-}
+// 	fmt.Println(searchInsert(randArr, 6))
+// }
 
-func searchInsert(nums []int, target int) int {
-	left := 0
-	right := len(nums) - 1
+// func searchInsert(nums []int, target int) int {
+// 	left := 0
+// 	right := len(nums) - 1
 
-	for left <= right {
-		middle := left + (right-left)/2
+// 	for left <= right {
+// 		middle := left + (right-left)/2
 
-		if nums[middle] == target {
-			return middle // Найдено целевое значение
-		} else if nums[middle] < target {
-			left = middle + 1 // Ищем в правой половине
-		} else {
-			right = middle - 1 // Ищем в левой половине
-		}
-	}
+// 		if nums[middle] == target {
+// 			return middle // Найдено целевое значение
+// 		} else if nums[middle] < target {
+// 			left = middle + 1 // Ищем в правой половине
+// 		} else {
+// 			right = middle - 1 // Ищем в левой половине
+// 		}
+// 	}
 
-	// Если не нашли, `left` указывает на место вставки
-	return left
-}
+// 	// Если не нашли, `left` указывает на место вставки
+// 	return left
+// }
---
+++ 419.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	board := [][]byte{
+		{'X', '.', '.', 'X'},
+		{'.', '.', '.', 'X'},
+		{'.', '.', '.', 'X'},
+	}
+
+	count := countBattleships(board)
+	fmt.Println(count) // Вывод количества кораблей
+}
+
+func countBattleships(board [][]byte) int {
+
+	countOfBattleships := 0
+	n := len(board)
+	m := len(board[0])
+
+	for i := 0; i < n; i++ {
+
+		for j := 0; j < m; j++ {
+
+			if board[i][j] == 'X' {
+				countOfBattleships++
+				dfs(board, i, j)
+			}
+
+		}
+	}
+	return countOfBattleships
+
+}
+
+func dfs(board [][]byte, i int, j int) {
+
+	if i < 0 || i >= len(board) || j < 0 || j >= len(board[0]) || board[i][j] == '.' {
+		return
+	}
+
+	board[i][j] = '.'
+
+	dfs(board, i+1, j)
+	dfs(board, i-1, j)
+	dfs(board, i, j+1)
+	dfs(board, i, j-1)
+
+}
